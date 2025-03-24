4c5bb4beaee56193e3fa37b162c48e132a70786f
monu <monubhagel90@gmail.com>
2025-03-25 01:12:38 +0530

    java syntax file added

---
+++ src/JavaSyntax.java
@@ -0,0 +1,16 @@
+public class JavaSyntax {
+    public static void main(String[] args){
+        System.out.println("hello world "+ "current file is java syntax");
+    }
+}
+
+
+/*
+- every line of code that is written to run in java must be inside a class.
+- and the class name should always start with uppercase , in our file its "JavaSyntax".
+- when saving the file the name of file must match the name of the class.
+- The curly braces {} marks the beginning and the end of a block of code.
+- System is a built-in Java class that contains useful members, such as out, which is short for "output".
+- in java each code statement must end with ";" semicolon
+- Java is case-sensitive .
+ */
