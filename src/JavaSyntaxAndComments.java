8d4c38b8886264dc66bdf5856675da9b66f64d38
monu <monubhagel90@gmail.com>
2025-03-25 12:11:57 +0530

    modified

--- src/JavaSyntaxAndComments.java
+++ src/JavaSyntaxAndComments.java
@@ -1,11 +1,17 @@
-public class JavaSyntax {
+public class JavaSyntaxAndComments {
     public static void main(String[] args){
         System.out.println("hello world "+ "current file is java syntax");
     }
 }
 
 
+// these are single line comments syntax.
+// to insert single line comment "double slash" are used "//".
+
+
 /*
+ this is a multi-line comment
+
 - every line of code that is written to run in java must be inside a class.
 - and the class name should always start with uppercase , in our file its "JavaSyntax".
 - when saving the file the name of file must match the name of the class.
@@ -14,3 +20,4 @@ public class JavaSyntax {
 - in java each code statement must end with ";" semicolon
 - Java is case-sensitive .
  */
+
